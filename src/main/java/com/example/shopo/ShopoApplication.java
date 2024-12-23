436d80609da67e7d8f11eec3fbcd5a48ae5f4a45
spint414 <nguyenvinhtrung1998@gmail.com>
2024-12-23 20:56:14 +0700

    Add images

--- src/main/java/com/example/shopo/ShopoApplication.java
+++ src/main/java/com/example/shopo/ShopoApplication.java
@@ -5,14 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javafx.scene.image.Image;
 import java.io.IOException;
 
 public class ShopoApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ShopoApplication.class.getResource("fxml/dashboard.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 800);
+        stage.setTitle("Store Management System");
+        stage.getIcons().add(new Image(getClass().getResource("/com/example/shopo/img/logo/shop-icon.png").toExternalForm()));
         stage.setScene(scene);
         stage.show();
     }
